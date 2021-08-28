be502bc5edc36398c6b515597d4593497514fc87
Brian Dukes <bdukes@engagesoftware.com>
2021-08-28 16:35:25 -0500

    Generate sitemap.xml

--- src/Api.elm
+++ src/Api.elm
@@ -4,11 +4,31 @@ import ApiRoute
 import DataSource exposing (DataSource)
 import Html exposing (Html)
 import Route exposing (Route)
+import Site
+import Sitemap
 
 
 routes :
     DataSource (List Route)
     -> (Html Never -> String)
     -> List (ApiRoute.ApiRoute ApiRoute.Response)
-routes _ _ =
-    []
+routes getStaticRoutes _ =
+    [ ApiRoute.succeed
+        (getStaticRoutes
+            |> DataSource.map
+                (\allRoutes ->
+                    { body =
+                        allRoutes
+                            |> List.map
+                                (\route ->
+                                    { path = Route.routeToPath route |> String.join "/"
+                                    , lastMod = Nothing
+                                    }
+                                )
+                            |> Sitemap.build { siteUrl = Site.config.canonicalUrl }
+                    }
+                )
+        )
+        |> ApiRoute.literal "sitemap.xml"
+        |> ApiRoute.single
+    ]
