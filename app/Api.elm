2545c87afd7cde602afc0cc03537e269ca9dd479
Brian Dukes <bdukes@engagesoftware.com>
2024-07-17 22:45:17 -0500

    Add RSS and sitemap back

--- app/Api.elm
+++ app/Api.elm
@@ -1,15 +1,97 @@
 module Api exposing (routes)
 
 import ApiRoute exposing (ApiRoute)
+import Article
 import BackendTask exposing (BackendTask)
 import FatalError exposing (FatalError)
 import Html exposing (Html)
+import Pages
 import Route exposing (Route)
+import Rss
+import Site
+import Sitemap
+import Time
 
 
 routes :
     BackendTask FatalError (List Route)
     -> (Maybe { indent : Int, newLines : Bool } -> Html Never -> String)
     -> List (ApiRoute ApiRoute.Response)
-routes getStaticRoutes htmlToString =
-    []
+routes getStaticRoutes _ =
+    [ rss
+        { siteTagline = Site.siteTagline
+        , siteUrl = Site.config.canonicalUrl
+        , title = "Out of Our Minds Articles"
+        , builtAt = Pages.builtAt
+        , indexPage = [ "articles" ]
+        }
+        postsDataSource
+    , ApiRoute.succeed
+        (getStaticRoutes
+            |> BackendTask.map
+                (\allRoutes ->
+                    allRoutes
+                        |> List.map
+                            (\route ->
+                                { path = Route.routeToPath route |> String.join "/"
+                                , lastMod = Nothing
+                                }
+                            )
+                        |> Sitemap.build { siteUrl = Site.config.canonicalUrl }
+                )
+        )
+        |> ApiRoute.literal "sitemap.xml"
+        |> ApiRoute.single
+    ]
+
+
+postsDataSource : BackendTask FatalError (List Rss.Item)
+postsDataSource =
+    Article.allMetadata
+        |> BackendTask.map
+            (List.map
+                (\( route, article ) ->
+                    { title = article.title
+                    , description = article.description
+                    , url =
+                        route
+                            |> Route.routeToPath
+                            |> String.join "/"
+                    , categories = []
+                    , author = article.author
+                    , pubDate = Rss.Date article.published
+                    , content = Nothing
+                    , contentEncoded = Nothing
+                    , enclosure = Nothing
+                    }
+                )
+            )
+
+
+rss :
+    { siteTagline : String
+    , siteUrl : String
+    , title : String
+    , builtAt : Time.Posix
+    , indexPage : List String
+    }
+    -> BackendTask FatalError (List Rss.Item)
+    -> ApiRoute.ApiRoute ApiRoute.Response
+rss options itemsRequest =
+    ApiRoute.succeed
+        (itemsRequest
+            |> BackendTask.map
+                (\items ->
+                    Rss.generate
+                        { title = options.title
+                        , description = options.siteTagline
+                        , url = options.siteUrl ++ "/" ++ String.join "/" options.indexPage
+                        , lastBuildTime = options.builtAt
+                        , generator = Just "elm-pages"
+                        , items = items
+                        , siteUrl = options.siteUrl
+                        }
+                )
+        )
+        |> ApiRoute.literal "articles/feed.xml"
+        |> ApiRoute.single
